dda05cc0a8f1fd5ef4f0e2e1bc877b0e0d7c0ce5
Ryan <ryankr@hotmail.co.uk>
2017-01-31 11:11:49 +0000

    Improved file organisation
    * Added some more hierarchy and redirected index.html’s form to the new
    location

--- Scripts/fetchData.php
+++ Scripts/fetchData.php
@@ -10,16 +10,19 @@
       die("Connection failed: " . $conn->connect_error);
   }
 
+  //forming Select statement for sources
   $sqlSources = "SELECT * FROM SourceAirports;";
   $sources = $conn->query($sqlSources);
 
+  //forming Select statement for destinations
   $sqlDestinations = "SELECT * FROM DestinationAirports;";
   $destinations = $conn->query($sqlDestinations);
 
+  //where i'll store the data
   $sourcesArr = array();
   $destinationsArr = array();
 
-
+  //storing the query results in more permanent storage
   while($row = mysqli_fetch_array($sources)){
     $sourcesArr[] = $row;
   }
@@ -28,6 +31,7 @@
     $destinationsArr[] = $row;
   }
 
+  //looping through to show how i'll get the pairings.
   foreach($sourcesArr as $row){
       echo "id: " . $row["SrcAirportID"]. " - Name: " . $row["SrcAirportName"] . "\n";
 
