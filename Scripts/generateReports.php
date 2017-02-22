3a4685ac3fe7dac03a5835a27111951fd5a561f7
Ryan Krol <ryankr@hotmail.co.uk>
2017-02-22 20:04:03 +0000

    Implemented a basic report generator
    * Currently the reports are going to be extremely basic, getting the
    flight that is the cheapest from all of the data, and just returning
    that to the end user.
    
    * This is something that will be polished a whole lot further later on,
    I just want a complete system for now.

--- Scripts/generateReports.php
+++ Scripts/generateReports.php
@@ -4,34 +4,49 @@
   include 'credentials.php';
   include 'Flight.php';
 
-  $redBound = 0.2;
-  $yellowBound = 0.35;
-  $greenBound = 0.5;
-  $hardCap = 150;
-  $minTripLength = 2;
-  $limitSize = 10;
+  // I don't currently need any of these variables
+  // $redBound = 0.2;
+  // $yellowBound = 0.35;
+  // $greenBound = 0.5;
+  // $hardCap = 150;
+  // $minTripLength = 2;
+  // $limitSize = 10;
 
 ?>
 <?php
   //functions
 
-  function getFlightsOfInterest($conn,$src,$dest,$bound,$previousPriceCap){
+  //this function will house the algorithm to get the flights that I'm interested in
+  function getFlightsOfInterest($conn,$src,$dest){
 
-    global $minTripLength;
-    global $hardCap;
-    global $limitSize;
-    $query = "SELECT * , (Price/(SELECT AveragePrice FROM Averages WHERE AirPort = '$dest')) FROM ${src}_${dest} WHERE Price < ($bound * (SELECT AveragePrice FROM Averages WHERE AirPort = '$dest')) AND DATEDIFF(ReturnDate,DepartDate) > $minTripLength AND Price < $hardCap AND Price > $previousPriceCap ORDER BY (Price/(SELECT AveragePrice FROM Averages WHERE AirPort = '$dest')) ASC limit $limitSize;";
+    //get the trip length from the query
+    // maybe get the cost per day from the query as well
+    // $query = "SELECT * , (Price/(SELECT AveragePrice FROM Averages WHERE AirPort = '$dest')) FROM ${src}_${dest} WHERE Price < ($bound * (SELECT AveragePrice FROM Averages WHERE AirPort = '$dest')) AND DATEDIFF(ReturnDate,DepartDate) > $minTripLength AND Price < $hardCap AND Price > $previousPriceCap ORDER BY (Price/(SELECT AveragePrice FROM Averages WHERE AirPort = '$dest')) ASC limit $limitSize;";
+
+    //this query gets the information of the flight that has the lowest cost/day
+    // $query = "SELECT *,DATEDIFF(ReturnDate,DepartDate) FROM BHX_MAD WHERE (Price/DATEDIFF(ReturnDate,DepartDate)) = (SELECT Min(Price/(DATEDIFF(ReturnDate,DepartDate))) from BHX_MAD WHERE DATEDIFF(ReturnDate,DepartDate) < 14) AND DATEDIFF(ReturnDate,DepartDate) <= 14;"
+
+    //this gets the flights that have the minimum price in the data set
+    // $query = "SELECT *, DATEDIFF(ReturnDate, DepartDate) FROM BHX_MAD Where Price = (Select Min(Price) from BHX_MAD);"
+
+    //for now i'm going to go with the cheapest flight because it will always return the same numner of flights
+
+    $query = "SELECT *, DATEDIFF(ReturnDate, DepartDate) FROM ${src}_${dest} Where Price = (Select Min(Price) from ${src}_${dest}) limit 1;";
 
     $pricesArray = arraySetup($conn,$query);
 
-    return $pricesArray;
+    //checking that some value was returned by the database
+    if (!isset($pricesArray[0])) {
+      $pricesArray[0] = NULL;
+    }
+
+    //[0] because I only want to return 1 result right now
+    return ($pricesArray[0]);
 
   }
 
   //this function will be used to setup my source and destination airport arrays
   function arraySetup($conn, $query){
-
-    // echo $query . "\n";
 
     $result = $conn->query($query);
 
@@ -47,10 +62,6 @@
 <?php
   //main
 
-  global $redBound;
-  global $yellowBound;
-  global $greenBound;
-
   // Create connection
   $conn = new mysqli($servername, $username, $password, $database);
 
@@ -62,34 +73,33 @@
   $sourcesArr = arraySetup($conn, "SELECT * FROM SourceAirports;");
   $destinationsArr = arraySetup($conn, "SELECT * FROM DestinationAirports;");
 
-  $redAlerts = array();
-  $yellowAlerts = array();
-  $greenAlerts = array();
+  $flightsArray = array();
+  $minItem = NULL;
 
-  foreach($sourcesArr as $srcAirport) {
-    foreach($destinationsArr as $destAirport) {
+  //getting a minimum for each destination, from all of the sources
+  foreach($destinationsArr as $destAirport) {
+    foreach($sourcesArr as $srcAirport) {
 
       $src = $srcAirport["SrcAirportCode"];
       $dest = $destAirport["DestAirportCode"];
 
-      array_merge($redAlerts, getFlightsOfInterest($conn,$src,$dest,$redBound,$hardCap,0));
-      array_merge($yellowAlerts, getFlightsOfInterest($conn,$src,$dest,$yellowBound,$hardCap,$redBound));
-      array_merge($greenAlerts, getFlightsOfInterest($conn,$src,$dest,$greenBound,$hardCap,$yellowBound));
+      $potentialMin = getFlightsOfInterest($conn,$src,$dest);
 
-    }
-  }
-
-  //at this point i'll have an array of red, yellow and green alerts
-  // so i want to send out a total of 15 reports. first priority will go to the red red alerts, then the yellow..
-
-  $reportsArray = array();
-
-  while(count($reportsArray) < 15){
-
-    $foreach($redAlerts as $redAlert){
-
+      //checks that the minItem and potentialMin variables aren't null
+      if(isset($minItem) && isset($potentialMin)){
+        if($potentialMin['Price'] < $minItem['Price']){
+          $minItem = $potentialMin;
+        }
+      } else {
+        if(isset($potentialMin)){
+          $minItem = $potentialMin;
+        }
+      }
     }
 
+    //add the minimimum item to the array of minimums
+    array_push($flightsArray, $minItem);
+    $minItem = NULL;
   }
 
   //close connection
